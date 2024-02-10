0cd9000ce03c53797595981d987b9e3c8bb0a089
sourceTree <sourcetree@mail.com>
2024-02-09 23:16:22 -0700

    05 - ApplicationContext and Property Initialization

--- springdemo/src/main/java/org/koushik/javabrains/DrawingApp.java
+++ springdemo/src/main/java/org/koushik/javabrains/DrawingApp.java
@@ -1,11 +1,11 @@
 package org.koushik.javabrains;
-import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
     public static void main(String[] args) {        
-        BeanFactory beanFactory = new ClassPathXmlApplicationContext("spring.xml");
-        Triangle triangle = (Triangle) beanFactory.getBean("triangle");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        Triangle triangle = (Triangle) applicationContext.getBean("triangle");
 
         triangle.draw();
     }
