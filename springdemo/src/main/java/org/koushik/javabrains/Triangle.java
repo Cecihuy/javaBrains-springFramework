70f07a0774bec0364b4cb6876a253adea54aba2f
sourceTree <sourcetree@mail.com>
2024-02-14 23:22:09 -0700

    12 - Using ApplicationContextAware

--- springdemo/src/main/java/org/koushik/javabrains/Triangle.java
+++ springdemo/src/main/java/org/koushik/javabrains/Triangle.java
@@ -1,10 +1,14 @@
 package org.koushik.javabrains;
-import java.util.List;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
-public class Triangle {
+public class Triangle implements ApplicationContextAware, BeanNameAware{
     private Point pointA;
     private Point pointB;
     private Point pointC;
+    private ApplicationContext context = null;
     
     public Point getPointA() {
         return pointA;
@@ -29,5 +33,13 @@ public class Triangle {
         System.out.println("Point = {" + getPointA().getX() +", " + getPointA().getY() + "}");
         System.out.println("Point = {" + getPointB().getX() +", " + getPointB().getY() + "}");
         System.out.println("Point = {" + getPointC().getX() +", " + getPointC().getY() + "}");
+    }
+    @Override
+    public void setApplicationContext(ApplicationContext context) throws BeansException {
+        this.context = context;
+    }
+    @Override
+    public void setBeanName(String beanName) {
+        System.out.println("Bean name is: " + beanName);
     }
 }
