a4e0631426f9112645334990b727f77b6b7c4c1f
sourceTree <sourcetree@mail.com>
2024-02-15 14:23:02 -0700

    18 - Introduction to Annotations and the Required Annotation(deprecated. Use autowired instead)

--- springdemo/src/main/java/org/koushik/javabrains/Circle.java
+++ springdemo/src/main/java/org/koushik/javabrains/Circle.java
@@ -1,11 +1,14 @@
 package org.koushik.javabrains;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Circle implements Shape{
     private Point center;
 
     public Point getCenter() {
         return center;
     }
+    @Autowired
     public void setCenter(Point center) {
         this.center = center;
     }
