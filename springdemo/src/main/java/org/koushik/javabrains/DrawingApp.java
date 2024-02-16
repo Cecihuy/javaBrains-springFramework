d20f67bfe825379c9a7016e8d992a6491fe5832e
sourceTree <sourcetree@mail.com>
2024-02-15 19:22:30 -0700

    22 - Using MessageSource To Get Text From Property Files

--- springdemo/src/main/java/org/koushik/javabrains/DrawingApp.java
+++ springdemo/src/main/java/org/koushik/javabrains/DrawingApp.java
@@ -1,13 +1,12 @@
 package org.koushik.javabrains;
-import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
     public static void main(String[] args) {        
-        AbstractApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
-        applicationContext.registerShutdownHook();
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
         Shape shape = (Shape) applicationContext.getBean("circle");
-
         shape.draw();
+        System.out.println(applicationContext.getMessage("greeting", null, "Default Greeting", null));
     }
 }
