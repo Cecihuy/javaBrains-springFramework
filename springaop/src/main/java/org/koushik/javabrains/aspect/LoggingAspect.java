a9ad1ff0a419ce2e83b115a1ee30f9b6074fb07f
sourceTree <sourcetree@mail.com>
2024-02-19 12:52:21 -0700

    28 - Pointcuts and Wildcard Expressions

--- springaop/src/main/java/org/koushik/javabrains/aspect/LoggingAspect.java
+++ springaop/src/main/java/org/koushik/javabrains/aspect/LoggingAspect.java
@@ -1,11 +1,23 @@
 package org.koushik.javabrains.aspect;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
-    @Before("execution(public String getName())")
+    // @Before("execution(public String org.koushik.javabrains.model.Circle.getName())")
+    // @Before("execution(public * get*())")
+    // @Before("execution(public * get*(*))")
+    // @Before("execution(public * get*(..))")
+    // @Before("execution(public * org.koushik.javabrains.model.*.get*())")
+    @Before(value = "allGetters()")
     public void loggingAdvice(){
         System.out.println("Advice run. Get method called");
     }
+    @Before(value = "allGetters()")
+    public void secondAdvice(){
+        System.out.println("Second Advice executed");
+    }
+    @Pointcut(value = "execution(* get*())")
+    public void allGetters(){}
 }
