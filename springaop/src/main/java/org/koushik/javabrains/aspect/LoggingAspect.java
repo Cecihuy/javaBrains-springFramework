a0128e09842b314d565a966a5d37245ce8f6c83f
sourceTree <sourcetree@mail.com>
2024-02-19 12:53:16 -0700

    29 - A Few More Pointcut Expressions

--- springaop/src/main/java/org/koushik/javabrains/aspect/LoggingAspect.java
+++ springaop/src/main/java/org/koushik/javabrains/aspect/LoggingAspect.java
@@ -4,20 +4,19 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
-public class LoggingAspect {
-    // @Before("execution(public String org.koushik.javabrains.model.Circle.getName())")
-    // @Before("execution(public * get*())")
-    // @Before("execution(public * get*(*))")
-    // @Before("execution(public * get*(..))")
-    // @Before("execution(public * org.koushik.javabrains.model.*.get*())")
-    @Before(value = "allGetters()")
+public class LoggingAspect {    
+    @Before(value = "allCircleMethods() && allGetters()")
     public void loggingAdvice(){
         System.out.println("Advice run. Get method called");
     }
-    @Before(value = "allGetters()")
+    // @Before(value = "allGetters()")
     public void secondAdvice(){
         System.out.println("Second Advice executed");
     }
     @Pointcut(value = "execution(* get*())")
     public void allGetters(){}
+    // @Pointcut(value = "execution(public * org.koushik.javabrains.model.Circle.*(..))")
+    // @Pointcut("within(org.koushik.javabrains.model..*)")
+    @Pointcut("within(org.koushik.javabrains.model.Circle)")
+    public void allCircleMethods(){}
 }
